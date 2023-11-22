2370d8e413080b75e228594fbd16aed48191edc1
titus <80385479+titusio@users.noreply.github.com>
2023-11-22 09:19:31 +0100

    Rename all config files to fit the project

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -6,42 +6,31 @@
 #include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/godot.hpp>
 
-#include "my_node.hpp"
-#include "my_singleton.hpp"
-
 using namespace godot;
 
-static MySingleton *_my_singleton;
 
-void gdextension_initialize(ModuleInitializationLevel p_level)
+void imguidot_initialize(ModuleInitializationLevel p_level)
 {
 	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE)
 	{
-		ClassDB::register_class<MyNode>();
-		ClassDB::register_class<MySingleton>();
-
-		_my_singleton = memnew(MySingleton);
-		Engine::get_singleton()->register_singleton("MySingleton", MySingleton::get_singleton());
 	}
 }
 
-void gdextension_terminate(ModuleInitializationLevel p_level)
+void imguidot_terminate(ModuleInitializationLevel p_level)
 {
 	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE)
 	{
-		Engine::get_singleton()->unregister_singleton("MySingleton");
-		memdelete(_my_singleton);
 	}
 }
 
 extern "C"
 {
-	GDExtensionBool GDE_EXPORT gdextension_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
+	GDExtensionBool GDE_EXPORT imguidot_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
 	{
 		godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-		init_obj.register_initializer(gdextension_initialize);
-		init_obj.register_terminator(gdextension_terminate);
+		init_obj.register_initializer(imguidot_initialize);
+		init_obj.register_terminator(imguidot_terminate);
 		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 		return init_obj.init();
